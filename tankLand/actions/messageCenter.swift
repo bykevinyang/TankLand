8cd8b0408ca7411b052fe367cf7a3d2ac243f475
Kevin Yang <45344788+bykevinyang@users.noreply.github.com>
2022-01-08 13:02:57 +0000

    final draft

---
+++ tankLand/actions/messageCenter.swift
@@ -0,0 +1,72 @@
+struct SendMessageAction: PreAction {
+    let action: ActionType
+    let id: String
+    let message: String
+    var description: String {
+        return "\(action) \(id) \(message)"
+    }
+    init(id: String, message: String){
+        action = .SendMessage
+        self.id = id
+        self.message = message
+    }
+}
+
+struct ReceiveMessageAction: PreAction{
+    let action: ActionType
+    let key: String
+    var description: String {
+        return "\(action) \(key)"
+    }
+    init(key: String){
+        action = .ReceiveMessage
+        self.key = key
+    }
+}
+
+struct MessageCenter {
+	static var messages: [String: String] = [:]
+
+	static func add(key: String, message: String) {
+    MessageCenter.messages[key] = message
+	}
+	
+	// Fix tank later
+	static func clear() {
+		MessageCenter.messages = [:]
+	}
+
+	static func read(key: String) -> String? {
+		//print("key")
+		if let secretMessage = MessageCenter.messages[key] {
+			return secretMessage
+		}
+		return nil
+	}
+}
+
+extension TankLand {
+	func sendMessage(tank: Tank, preAction: PreAction) -> Bool {
+		if preAction.action == .SendMessage {
+			let action = preAction as! SendMessageAction
+			MessageCenter.add(key: action.id, message: action.message)
+			print("ADDED MESSAGE TO MESSAGE CENTER")
+			print(MessageCenter.messages)
+			return true
+		}
+    return false
+	}
+
+	func receiveMessage(tank: Tank, preAction: PreAction) -> Bool {
+		print("A TANK WANTS TO READ A MESSAGE")
+		if preAction.action == .ReceiveMessage {
+			let action = preAction as! ReceiveMessageAction
+			let message = MessageCenter.read(key: action.key)
+			print("MESSAGE: \(message)")
+			tank.receivedMessage = message
+			print("READING")
+			return true
+  	}
+    return false
+	}	
+}
